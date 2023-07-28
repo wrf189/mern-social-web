8dd6ceae2945aad17fd814997fc6d31eb72690ef
wrf189 <74730980+wrf189@users.noreply.github.com>
2023-07-29 05:00:23 +0700

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3001/auth/register",
+      "https://mernsocialweb-wangsa-api.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
